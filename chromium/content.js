3ab7fa5ff80e47a0a37d65b7a2f1f476045a618c
Joshua Fan <joshuaptfan@gmail.com>
2018-07-14 09:32:03 -0700

    Implement probabilistic text replacement, popup mapping preview

--- chromium/content.js
+++ chromium/content.js
@@ -1,10 +1,12 @@
 var isEnabled;
 var mapping;
+var randSeed;
 
 // Retrieve data from background script on load
 chrome.runtime.sendMessage({ name: 'getData' }, items => {
 	window.isEnabled = items.isEnabled;
 	window.mapping = items.mapping;
+	genRandSeed();
 	processText(document.body, isEnabled ? 2 : 0);
 
 	// Parse DOM on change
@@ -26,6 +28,7 @@ document.addEventListener('visibilitychange', () => {
 	chrome.runtime.sendMessage({ name: 'getData' }, items => {
 		window.isEnabled = items.isEnabled;
 		window.mapping = items.mapping;
+		genRandSeed();
 		processText(document.body, isEnabled ? 3 : 1);
 	});
 });
@@ -39,51 +42,78 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 		case 'mapping':
 			window.mapping = msg.mapping;
 	}
+	genRandSeed();
 	processText(document.body, isEnabled ? 3 : 1);
 });
 
 // Recursively process text within descendent text nodes
 function processText(parentNode, mode) {
-	for (var node of parentNode.childNodes) {
+	for (var node of parentNode.childNodes)
 		switch (node.nodeType) {
-			case 1:    // element node
-			case 11:   // document fragment node
+			case 1:    // Element node
+			case 11:   // Document fragment node
 				if (!/SCRIPT|STYLE/.test(node.nodeName))
 					processText(node, mode);
 				break;
-			case 3:    // text node
+			case 3:    // Text node
 				switch (mode) {
-					case 0:    // cache text
+					case 0:    // Cache text
 						node.originalText = node.nodeValue;
 						break;
-					case 1:    // restore text
+					case 1:    // Restore text
 						node.nodeValue = node.originalText;
 						break;
-					case 2:    // cache and replace text
+					case 2:    // Cache and replace text
 						node.originalText = node.nodeValue;
-					case 3:    // replace text
+					case 3:    // Replace text
 						let str = node.originalText || node.nodeValue;
 						let out = '';
 						for (var i = 0, sLen = str.length; i < sLen; i++) {
 							if (!mapping[str[i]])
 								out += str[i];
 							else {
+								// Search trie
 								let currentNode = mapping[str[i]];
 								let depth = 0;
 								let result = '';
 								while (true) {
-									if (currentNode.value)
-										result = currentNode.value;
+									if (currentNode.value) {
+										if (typeof currentNode.value == 'string')
+											// Single result
+											result = currentNode.value;
+										else {
+											// Probalistic result pool
+											let rand = nextRand();
+											for (var val in currentNode.value) {
+												rand -= currentNode.value[val];
+												if (rand < 0) {
+													result = val;
+													break;
+												}
+											}
+										}
+									}
 									if (currentNode[str[i + depth + 1]])
 										currentNode = currentNode[str[i + ++depth]];
 									else break;
 								}
-								out += result || str.slice(i, i + depth + 1);
+								// Insert original text if match is incomplete
+								out += result || str.substr(i, depth + 1);
 								i += depth;
 							}
 						}
 						node.nodeValue = out;
 				}
 		}
-	}
+}
+
+// Generate PRNG seed from URL
+function genRandSeed() {
+	randSeed = location.href.split('').reduce((v, c) => v + c.codePointAt(), 0) * 16807
+}
+
+// Generate next random number in range [0, 1)
+function nextRand() {
+	randSeed = randSeed * 16807 % 0x7FFFFFFF;
+	return (randSeed - 1) / 0x7FFFFFFF;
 }
--- chromium/popup.js
+++ chromium/popup.js
@@ -2,10 +2,13 @@ document.onreadystatechange = function () {
 	document.getElementById('toggle-enabled').addEventListener('change', () => { setIsEnabled(!window.isEnabled); });
 	document.getElementById('set-mapping').addEventListener('change', e => { setMapping(e.target.value) });
 
+	var background = chrome.extension.getBackgroundPage();
+
 	// Retrieve data from local storage
 	chrome.storage.local.get(null, items => {
 		setIsEnabled(items.isEnabled || items.isEnabled === undefined);
 		document.getElementById('set-mapping').value = items.fileName;
+		showMapping(items.fileName);
 	})
 };
 
@@ -25,4 +28,47 @@ function setIsEnabled(isEnabled) {
 // Tell background script to load file
 function setMapping(fileName) {
 	chrome.runtime.sendMessage({ name: 'loadMapping', fileName });
+	showMapping(fileName);
+}
+
+function showMapping(fileName) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'mappings/' + fileName + '.json', true);
+	xhr.onload = () => {
+		let mapping = JSON.parse(xhr.responseText);
+		let editMapping = document.getElementById('edit-mapping');
+		while (editMapping.childNodes.length)
+			editMapping.removeChild(editMapping.lastChild);
+		for (key in mapping) {
+			const branch = document.createElement('div');
+			branch.className = 'mapping-branch';
+			editMapping.appendChild(branch);
+			const divKey = document.createElement('div');
+			divKey.className = 'mapping-key';
+			divKey.textContent = key;
+			branch.appendChild(divKey);
+			if (typeof mapping[key] == 'string')
+				// Single result with 100% chance
+				showValue(branch, mapping[key], 1);
+			else
+				// Probabilistic result pool
+				for (val in mapping[key])
+					showValue(branch, val, mapping[key][val]);
+		}
+	};
+	xhr.send();
+}
+
+function showValue(container, value, probability) {
+	const row = document.createElement('div');
+	row.className = 'mapping-value-row';
+	const val = document.createElement('div');
+	val.className = 'mapping-value';
+	val.textContent = value;
+	row.appendChild(val);
+	const prob = document.createElement('div');
+	prob.className = 'mapping-probability';
+	prob.textContent = probability * 100 + '%';
+	row.appendChild(prob);
+	container.appendChild(row);
 }
